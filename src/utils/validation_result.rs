b32258c3144b0575c3647861905c194b58c06684
James Cauwelier <james@accelerated.dev>
2024-06-28 15:13:17 -0700

    Removes unused code

--- src/utils/validation_result.rs
+++ src/utils/validation_result.rs
@@ -1,16 +1,3 @@
 use crate::utils::validation_error::ValidationError;
 
 pub type ValidationResult<T> = Result<T, ValidationError>;
-
-pub(crate) trait ValidationResultValue<T> {
-    fn get(&self) -> Option<&T>;
-}
-
-impl<T> ValidationResultValue<T> for ValidationResult<T> {
-    fn get(&self) -> Option<&T> {
-        match self {
-            Ok(value) => Some(value),
-            Err(_) => panic!("Cannot get the value of an error result.")
-        }
-    }
-}
